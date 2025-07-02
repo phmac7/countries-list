43a6aae5ca280d01490b55b49753905c662dd4aa
Pedro Machado <phvieiras@hotmail.com>
2025-07-01 23:46:48 -0300

    feat: allow cache

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,19 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
